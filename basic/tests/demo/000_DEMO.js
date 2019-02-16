9d812ec59ac6593d63e51cb799dc53849367f6f6
Adrian Lewis <spurty@gmail.com>
2019-02-16 08:49:09 -0500

    Tweaking

--- basic/tests/demo/000_DEMO.js
+++ basic/tests/demo/000_DEMO.js
@@ -1,7 +1,21 @@
 console.log('#000 TEST GOOGLE EXISTS');
-describe('#000 TEST GOOGLE Home page', ()=>{
-   it('#000_1 TEST PAGE EXISTS', function(){
-       console.log('#000_1a TESTING GOOGLE.COM can be reached by ');
-       browser.url('http://google.com');      
-   });  
+var URL='https://google.com';
+describe('#000 TEST GOOGLE Home page', function()
+{
+
+  it('#000_1 TEST PAGE EXISTS', function()
+  {
+    console.log('#000_1a TESTING GOOGLE.COM can be reached by ');
+    browser.url(URL);
+  });
+
+  it('#000_2 TEST PAGE EXISTS', function()
+  {
+    var GetTitle = browser.getTitle().then(function(promisedResult)
+    {
+      console.log("Title is : " + promisedResult);
+      return promisedResult;
+    })
+  });
+
 });
