50ee0945d4c0fb3817322b551053f3006d77a881
Adrian Lewis <spurty@gmail.com>
2019-02-20 18:34:40 -0500

    Remove redundancy

--- basic/tests/demo/000_DEMO.js
+++ basic/tests/demo/000_DEMO.js
@@ -21,21 +21,4 @@ describe('#001 TESTS', function(){
     
   });
 
-  it('#001-03 Stay on Google.com', function()
-  {   
-    var GetTitle = browser.getTitle().then(function(promisedResult)
-    {
-      console.log("#001-03 Title should still be 'Google' ");
-      assert.equal(promisedResult, 'Google');
-      return promisedResult;
-    })
-  });
-
-  it('#001-04 Search Google.com', function()
-  {   
-    console.log("#001-04a Locate id='q'");
-    console.log("#001-04b Submit click() on this element");
-    console.log("#001-04c report something maybe?");  
-  });
-
 });
