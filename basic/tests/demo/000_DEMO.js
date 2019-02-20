5c75d4b625bddd14f2d91f23153c18bba4f21674
Adrian Lewis <spurty@gmail.com>
2019-02-20 18:57:03 -0500

    Update 000_DEMO.js

--- basic/tests/demo/000_DEMO.js
+++ basic/tests/demo/000_DEMO.js
@@ -1,7 +1,6 @@
 var URL='http://google.com';
 const assert = require('assert');
-const { remote } = require('webdriverio')
-
+ 
 describe('#001 TESTS', function(){
 
   it('#001-01 Navigate to http://google.com', function()
@@ -18,7 +17,15 @@ describe('#001 TESTS', function(){
       assert.equal(promisedResult, 'Google');
       return promisedResult;
     })
-    
+  });
+
+  it('#001-03 Search on Google', function()
+  {
+    /*
+      How do we locate the search button and pass it a Mouse click?       
+      Used to just  `browser.click("name=q");`
+      But that fails with 'TypeError: browser.click is not a function'     
+    */
   });
 
 });
