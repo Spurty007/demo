106435f92bf24945afe06105b847c35cb6562d25
Adrian Lewis <spurty@gmail.com>
2019-02-24 20:27:51 -0500

    nope

--- basic/tests/PO/GooglePO.js
+++
@@ -1,7 +0,0 @@
-//import { browser, element, by } from 'protractor' ;
-import * from 'protractor';
-export class GooglePO {
-    public static searchBar = element(by.name("q"))
-    public static googleSearch = element(by.name("btnK"))
-    feelingLucky(){  return element(by.name("btnI")) }
-}
